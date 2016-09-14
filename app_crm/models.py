57a95a14f9cc76d7a603549865b55e2f03833e79
williamjohngardner <bill@williamjohngardner.com>
2016-09-14 18:03:55 -0400

    Scaffolded Models

--- app_crm/models.py
+++ app_crm/models.py
@@ -1,14 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
-# user company
+class Asset(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True) # validators should be a list
+    email = models.EmailField()
+    website = models.UrlField(max_length=300)
+    street = CharField(max_length=50)
+    street2 = CharField(max_length=20)
+    city = CharField(max_length=30)
+    state = CharField(max_length=4)
+    country = CharField(max_length=30)
 
-# Extend user model
+class User(models.Model):
+    asset = models.ForeignKey(Asset)
+    user = models.ForeignKey("auth.User")
 
-# contact company
+class Company(models.Model):
+    users = models.ForeignKey("auth.User")
+    name = models.CharField(max_length=50)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True) # validators should be a list
+    website = models.UrlField(max_length=300)
+    street = CharField(max_length=50)
+    street2 = CharField(max_length=20)
+    city = CharField(max_length=30)
+    state = CharField(max_length=4)
+    country = CharField(max_length=30)
 
-# contact
+class Customer(models.Model):
+    asset = models.ForeignKey(Asset)
+    notes = models.TextField(null=True, blank=True)
+    # Other fields can be applied here... just need to flesh out this functionality
+    # What things do you need to know about a customer that are not in the company level or the asset level?
 
-# contact tags
+class Orginization(models.Model):
+    customers = models.ForeignKey(Customer)
+    # Also not completely sure of full functionality here.
 
-# US States?
+class Note(models.Model):
+    users = models.ForeignKey("auth.User")
+
+class Task(models.Model):
+    users = models.ForeignKey("auth.User")
+
+class Tag(models.Model):
+    users = models.ForeignKey("auth.User")
