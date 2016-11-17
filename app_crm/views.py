160f1a9299b152383f8ce70abb4ec2deb05493d0
williamjohngardner <bill@williamjohngardner.com>
2016-11-17 13:38:27 -0500

    Creating Asset Views

--- app_crm/views.py
+++ app_crm/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 
-from django.views.generic import TemplateView, CreateView
-# from app_crm.models import Customer
+from django.views.generic import TemplateView, CreateView, ListView
+from app_crm.models import Asset
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
-class CreateCustomerView(CreateView):
-    # model = Customer
-    fields = ['asset', 'notes']
+class CreateAssetView(CreateView):
+    model = Asset
+    fields = ['user', 'is_company', 'first_name', 'last_name', 'company', 'phone_number', 'email', 'street', 'street2', 'city', 'state', 'zip_code', 'country', 'website', 'twitter', 'facebook', 'linkedin', 'profile_picture']
     success_url = reverse_lazy("index_view")
 
     def form_valid(self, form):
@@ -18,4 +18,5 @@ class CreateCustomerView(CreateView):
         customer.user = self.request.user
         return super(CreateCustomerView, self).form_valid(form)
 
-#'first_name', 'last_name', 'phone_number', 'email', 'website', 'street', 'street2', 'city', 'state', 'country',
+class AssetListView(ListView):
+    model = Asset
--- crm_project/urls.py
+++ crm_project/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
-from app_crm.views import IndexView, CreateCustomerView
+from app_crm.views import IndexView, CreateAssetView, AssetListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', IndexView.as_view(), name="index_view"),
-    url(r'^create_customer$', CreateCustomerView.as_view(), name="create_customer_view")
+    url(r'^create_asset$', CreateAssetView.as_view(), name="create_asset_view"),
+    url(r'^asset_list$', AssetListView.as_view(), name="asset_list_view")
 ]
