5c26217d66e011ef5c870a85df93ee64cab97583
williamjohngardner <bill@williamjohngardner.com>
2016-11-18 16:10:38 -0500

    Iron Pints

--- crm_project/urls.py
+++ crm_project/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', AssetListView.as_view(), name="asset_list_view"),
     url(r'^create_asset$', CreateAssetView.as_view(), name="create_asset_view"),
-    # url(r'^asset_list$', AssetListView.as_view(), name="asset_list_view"),
-    url(r'^asset_list/(?P<pk>\d+)/$', AssetDetailView.as_view(), name="asset_detail_view")
+    url(r'^index$', IndexView.as_view(), name="index_view"),
+    url(r'^asset_list/(?P<pk>\d+)/$', AssetDetailView.as_view(), name="asset_detail_view"),
+    url(r'^calendar/', include('schedule.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
