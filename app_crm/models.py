266733ad8e06ca9b043e3915b5b5f6c538cf1437
williamjohngardner <bill@williamjohngardner.com>
2016-11-30 11:11:43 -0500

    updated null blank fields

--- app_crm/models.py
+++ app_crm/models.py
@@ -14,25 +14,25 @@ class Company(models.Model):
 
 
 class Asset(models.Model):
-    user = models.ForeignKey("auth.User", null=True, blank=True)
+    user = models.ForeignKey("auth.User", blank=True)
     is_company = models.BooleanField(default=False, verbose_name='Is This a Company?')
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    company = models.ForeignKey(Company, null=True, blank=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    company = models.ForeignKey(Company, blank=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = PhoneNumberField(blank=True) # https://django-localflavor.readthedocs.io/en/latest/localflavor/us/
-    email = models.EmailField(blank=True, null=True)
-    street = models.CharField(max_length=50, blank=True, null=True)
-    street2 = models.CharField(max_length=50, blank=True, null=True)
-    city = models.CharField(max_length=30, blank=True, null=True)
-    state = models.CharField(max_length=4, blank=True, null=True)
-    zip_code = models.CharField(max_length=10, blank=True, null=True)
-    country = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    street2 = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    state = models.CharField(max_length=4, blank=True)
+    zip_code = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=30, blank=True)
     website = models.URLField(max_length=350, blank=True, null=True)
     twitter = models.URLField(max_length=350, blank=True, null=True)
     facebook = models.URLField(max_length=350, blank=True, null=True)
     linkedin = models.URLField(max_length=350, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to="profile_images", blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_images", blank=True)
 
     def __str__(self):
         return '{}, {}'.format(self.last_name, self.first_name)
@@ -46,18 +46,18 @@ class Asset(models.Model):
 
 
 class Note(models.Model):
-    note_creator = models.ForeignKey('auth.User', null=True, blank=True)
+    note_creator = models.ForeignKey('auth.User')
     note_is_about = models.ForeignKey(Asset)
     note = models.TextField()
-    note_picture = models.ImageField(upload_to="note_images", blank=True, null=True)
-    note_file = models.FileField(upload_to='note_files', blank=True, null=True)
+    note_picture = models.ImageField(upload_to="note_images", blank=True)
+    note_file = models.FileField(upload_to='note_files', blank=True)
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}, {}'.format(self.note_creator, self.note_is_about)
 
 class Task(models.Model):
-    creator = models.ForeignKey('auth.User', null=True, blank=True)
+    creator = models.ForeignKey('auth.User', blank=True)
     assigned_to = models.ForeignKey('auth.User', related_name="Assignee")
     task_is_about = models.ForeignKey(Asset)
     task = models.TextField()
@@ -72,6 +72,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return '{}, {}'.format(self.user, self.tag)
+
 
 @receiver(post_save, sender='auth.User')
 def create_user_profile(**kwargs):
